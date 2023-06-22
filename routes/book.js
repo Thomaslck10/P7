ac6e90f52dfa4902bfe6df70c2af92c77c4c0e41
Thomas Luck <thomasluck10@hotmail.fr>
2023-06-22 15:13:14 +0200

    développe fonction delete et modify

--- routes/book.js
+++ routes/book.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const auth = require('auth');
 const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 
 const bookCtrl = require('../controllers/book');
 
-router.post('/', auth, bookCtrl.createBook);
-router.put('/:id', auth, bookCtrl.modifyBook);
+router.post('/', auth, multer, bookCtrl.createBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/:id', bookCtrl.getOneBook); 
 router.get('/', bookCtrl.getAllBooks);
