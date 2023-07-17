d07ccf24e4c1a7e349129dcfa91f35e831d54852
Thomas Luck <thomasluck10@hotmail.fr>
2023-07-17 15:47:49 +0200

    update sharp + multer

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -11,9 +11,10 @@ const storage = multer.diskStorage({
         callback(null, 'images')
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        const fileName = file.originalname.split('.');
+        const name = fileName[0].split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
+        callback(null, name + Date.now() + '.webp');
     }
 });
 
